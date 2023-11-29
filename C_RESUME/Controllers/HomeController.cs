90ec4352e77819a1f006780db15cf6cdce7a3c48
CC <tog963812@gmail.com>
2023-11-29 11:16:55 +0800

    Create_作品集

--- C_RESUME/Controllers/HomeController.cs
+++ C_RESUME/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using C_RESUME.Models;
 using C_RESUME_DTO.CommonDTO;
+using C_RESUME_SYS_UserManagerServices;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,22 +8,54 @@ namespace C_RESUME.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly ISYS_UserManagerServices _ISYSUserManagerServices;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ISYS_UserManagerServices ISYS_UserManagerServices)
         {
-            _logger = logger;
+            this._ISYSUserManagerServices = ISYS_UserManagerServices;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        //public IActionResult Login() //待開發..後台
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult Logins(SignDTO DTO)
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult SignUp(SignDTO DTO)
+        //{
+        //    object[] result = _ISYSUserManagerServices.CreateSignUpData(DTO);
+        //    if ((bool)result[0])
+        //    {
+        //        ViewBag.result = "true";
+        //        ViewBag.resultMsg = result[1];
+        //        return View("Login");
+        //    }
+        //    ViewBag.result = "false";
+        //    ViewBag.resultMsg = result[1];
+        //    return View("Login");
+        //}
+
+
         [HttpPost]
         public IActionResult ViewJobExperi(string viewName)
         {
             return PartialView(viewName);
         }
+        [HttpPost]
+        public IActionResult ViewPortfolio(string viewName)
+        {
+            return PartialView(viewName);
+        }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
